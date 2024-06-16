56a85fa0c8e4d11ecd2c3a4f3ee4f2e9b424ed43
Zaheer Ahmad <zaheerahmad@falcon.local>
2024-06-16 18:54:53 +0500

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
-echo "hello"
+
+set -e
+if ! command -v docker &> /dev/null; then
+    echo "Docker is not installed. Installing Docker..."
+    curl -fsSL https://get.docker.com | bash -
+else
+    echo "Docker is already installed."
+fi
+
+# Check Docker version
+docker --version
+
+# Check docker-compose version
+docker compose version
+
+cd calcom/nginx-proxy
+
+echo "Stopping and removing existing docker-compose containers..."
+sudo docker-compose down || true  # Continue on error (if no containers are running)
+
+echo "Starting docker-compose..."
+sudo docker-compose up -d
